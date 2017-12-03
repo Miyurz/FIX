ef9570652695565e60bafcd179088a3146466e65
MiyurZ <mayur.nagekar@gmail.com>
2017-12-03 14:45:48 +0530

    Working copy

--- fix.py
+++ fix.py
@@ -1,151 +1,146 @@
 import sys
+import paramiko
 import os
 import time
-import time
-import datetime
-import matplotlib
 import pandas as pd
 from collections import Counter
+import matplotlib.pyplot as plt 
+import matplotlib.dates as mdates
+import datetime
+import dateutil
+import numpy as np
 
-fileName = "cme-msg-trace-cl-CME-2016-04-14T21_24_48.086Z.db"
+fileName = ""
 
-def downloadRecentFixFile(hostname, username,port, password):
+def downloadRecentFixFile():
     global fileName
-    fileName = "cme-msg-trace-cl-CME-2016-04-14T21_24_48.086Z.db" 
-    # mykey = paramiko.RSAKey.from_private_key_file('~/My-ssh.priv')  # This is when password less login is setup
-    password = password                                             # This is used when password is used to login      
-    host = hostname
-    username = username
-    port = port
 
-    transport = paramiko.Transport((host, port))
+    #Export these variables.
+    password = os.environ.get('ftp_password')                                    
+    host = os.environ.get('ftp_host')                                                   
+    username = os.environ.get('ftp_username')                             
+    port = 22 
     
-    transport.connect(username = username, password = password) # This is used when password is used to login
+    #password = "password" 
+    #host = "test.rebex.net"
+    #username = "demo"                           
     
-    sftp = paramiko.SFTPClient.from_transport(transport)
-    sftp.chdir('outgoing')
+    paramiko.util.log_to_file("paramiko.log")
+    try:
+        print password
+        print username
 
+        transport = paramiko.Transport((host, port))
+        transport.connect(username = username, password = password)
+        #sftp = paramiko.SFTPClient.from_transport(transport)
+        #sftp.chdir('artefacts/')
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
+    else:
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        #sftp.chdir('artefacts/')
+        sftp.chdir('/data/sample')
+    finally:
+        print "Cleaning up the stuff before leaving try"
+    
+    #sftp.chdir('artefacts/')
+
+    '''
+         Contents of artefacts directory on ftp server
+         -rwxr-xr-x  1 mnagekar  admins  2397869 Dec  2 17:42 cme-msg-trace-cl-CME-2016-04-14T21_24_48.086Z.db
+         -rwxr-xr-x  1 mnagekar  admins  2397869 Dec  2 16:12 cme-msg-trace-cl-CME-2016-04-14T21_24_48.087Z.db
+         -rwxr-xr-x  1 mnagekar  admins  2397869 Dec  2 16:15 cme-msg-trace-cl-CME-2016-04-14T21_24_48.088Z.db
+         -rwxr-xr-x  1 mnagekar  admins  2397869 Dec  2 16:18 cme-msg-trace-cl-CME-2016-04-14T21_24_48.089Z.db
+         -rwxr-xr-x  1 mnagekar  admins  2397869 Dec  2 16:19 cme-msg-trace-cl-CME-2016-04-14T21_24_48.090Z.db
+
+    '''
+
+    latest = 0
+    latestfile = None
+    
     for fileattr in sftp.listdir_attr():
         try:
             if fileattr.filename.startswith('cme-msg') and fileattr.st_mtime > latest:
                 latest = fileattr.st_mtime
                 latestfile = fileattr.filename
-                print "Downloading file ==> " + latestfile
-                sftp.get(filename, localpath)
+                if latestfile is not None:
+                    fileName = './' + latestfile
+                    print "Downloading file ==> " + fileName
+                    sftp.get(fileName, fileName)
+                else:
+                    print "ERROR: Didn't find the file to download"
+                    sys.exit(1)
+
         except IOError as e:
             print e
     sftp.close()
     transport.close()
-
-    for filename in sftp.listdir():
-        try:
-            if filename.startswith('cme-msg-trace-cl-CME-%s.csv'%formattedtime):
-                localpath= destination + '/' + filename
-                print "Downloading files ==> " + filename
-                sftp.get(filename, localpath)   
-        except IOError as e:
-            print e
-    sftp.close()
-    transport.close()
-
 
 def calculateOrderSingle():
-    #print(inspect.currentframe().f_code.co_name)
+    print fileName
+    
     SOH='\x01'
     NEWLINE="\n"
     DELIMITER='='
     SENDING_TIME_TAG="52"
-    x = []
-    y = []
+    msgTimestamp = []
     time_slot = open('time_slot.txt', 'w') 
+    
     with open(fileName) as FileObj:
         for line in FileObj:
-            #print("New line is"+line)
             line = line.strip("\n").split(SOH)
-            #print ("Sanitised line is "+str(line))
-            #print "Number of items in line="+str(len(line))
             tag = ""
             value = ""
             for pair in line:
                 if pair is not "":
-                    #print("pair is "+pair)
-                    #print pair.split(DELIMITER)
                     tag =  pair.split(DELIMITER)[0]
                     value = pair.split(DELIMITER) [1]
-                    #print("tag is "+tag)
-                    #print("value is "+value)
                     if tag == SENDING_TIME_TAG:
-                        #print("value = "+value)
                         myDate = value.replace("-", " ")
                         #20160415-02:02:08.678
-                        #print myDate
                         dt_obj = datetime.datetime.strptime(myDate,"%Y%m%d %H:%M:%S.%f")
                         dt_str = datetime.datetime.strftime(dt_obj,"%Y-%m-%d %H:%M:%S")
-                        #print dt_str
 
                         #dt = time.mktime(dt.timetuple()) + (dt.microsecond / 1000000.0)
-                        #dt = time.mktime(dt.timetuple())
-                        x.append(dt_str)
-                        #y.append(myDate)
-                        #print>>time_slot,("{0:.3f}".format(tmp))
-                        #print>>time_slot, tmp
+                        msgTimestamp.append(dt_str)
 
-    #print("Number of items = "+str(len(x)))
-    d = Counter(x)
-    n, m = d.keys(), d.values()
-    #print n, m
-    #print("Number of items in keys = "+str(len(n)))
-    #print("Number of items in values = "+str(len(m)))
-    #for f, b in zip(n, m):
-        #print "key = %.1f value = %.1f" % (f, b)
-        #print "{} Order single message(s) sent at  timestamp {}".format(b,f)
-    #return n,m
-    #printStats(m,n)
+    keyValue = Counter(msgTimestamp)
+    n, m = keyValue.keys(), keyValue.values()
+    for f, b in zip(n, m):
+        print "{} Order single message(s) sent at  timestamp {}".format(b,f)
+    printStats(n,m)
     plotData(n,m)
+    
     #frequency_msg = open('frequency_msg.txt', 'w')
     #print>>frequency_msg,dict((X,x.count(X)) for X in set(x))
-    #time.sleep(0)
-    #print("\n")
 
-def printStats(list1, list2):
-    print list1
-    print list2
-    #print set(list2)
-    #print set(list1)
-
-def plotData(list1, list2):
-    import matplotlib.pyplot as plt 
-    import matplotlib.dates as mdates
-    import datetime as dt
-    import dateutil
-    print list1
-    print list2
-
-    x = [dt.datetime.strptime(d,'%Y-%m-%d %H:%M:%S').date() for d in list1]
-    #x = [datetime.datetime.strptime('{:08}'.format(int(date)),'%Y%m%d %H:%M:%S') for date in list1]
+def printStats(x, y):
     
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y %H:%M:%S'))
-    plt.gca().xaxis.set_major_locator(mdates.HourLocator())
-    plt.plot(x, list2)
-    plt.gcf().autofmt_xdate()
-
-    #dates = matplotlib.dates.date2num(list1)
-    #print dates
-    #matplotlib.pyplot.plot_date(dates, list2)
+    print("Median for message rate => {} ".format(np.median(y)))
     
-    #plt.scatter(list1, list2)
-    #dates = [pd.to_datetime(d) for d in list1]
-    #print dates
-    #print list2
-    #plt.scatter(dates, list2, s =100, c = 'red')
+    print("99 percentile => {} ".format(np.percentile(y, 99)))
+    
+    print("Max => {} ".format(np.max(y)))
+    
+    print("Min => {}".format(np.min(y)))
+
+def plotData(x, y):
+
+    x1 = [datetime.datetime.strptime(d,'%Y-%m-%d %H:%M:%S').date() for d in x]
+    
     plt.title('Plotting graph between time stamp and message rate')
     plt.xlabel('Timestamp')
     plt.ylabel('Message rate')
-    #plt.xlim(0,5)
-    #plt.ylim(0,100000)
+    
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y %H:%M:%S'))
+    plt.gca().xaxis.set_major_locator(mdates.HourLocator())
+    
+    plt.plot(x1, y)
+    plt.gcf().autofmt_xdate()
+    
     plt.show()
 
-
-downloadRecentFixFile()
-calculateOrderSingle()
-#list1, list2 = calculateOrderSingle()
+if __name__ == '__main__':
+    downloadRecentFixFile()
+    calculateOrderSingle()
